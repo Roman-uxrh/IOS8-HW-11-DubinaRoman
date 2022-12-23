398c843bcd179ab9df7974eb3db132623e83a61b
RRDubina <dubinaroman.dubina@gmail.com>
2022-12-23 20:14:46 +0500

    Add TextField (name) + 2 icon

--- IOS8-HW-11-DubinaRoman/Sourse/ViewController.swift
+++ IOS8-HW-11-DubinaRoman/Sourse/ViewController.swift
@@ -21,6 +21,19 @@ class ViewController: UIViewController {
         return labelLogin
     }()
     
+    private lazy var userNameTF: UITextField = {
+        let textField = UITextField()
+        textField.textColor = .black
+        textField.textAlignment = .left
+        textField.placeholder = "Your name"
+        textField.font = UIFont(name: "Inter-Regular", size: 14)
+        textField.backgroundColor = .white
+        textField.layer.cornerRadius = 25
+        textField.setLeftIcon(UIImage(systemName: "person")!)
+        textField.setRightIcon(UIImage(systemName: "checkmark.circle.fill")!)
+        return textField
+    }()
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
@@ -38,6 +51,7 @@ class ViewController: UIViewController {
     
     private func setupHierarchy() {
         view.addSubview(labelLogin)
+        view.addSubview(userNameTF)
     }
     
     private func setupLayout() {
@@ -46,8 +60,38 @@ class ViewController: UIViewController {
             make.centerX.equalTo(view)
             make.top.equalTo(view).offset(100)
         }
+        
+        userNameTF.snp.makeConstraints { make in
+            make.top.equalTo(labelLogin.snp.bottom).offset (50)
+            make.height.equalTo(50)
+            make.left.equalTo (view).offset(40)
+            make.right.equalTo(view).offset(-40)
+        }
     }
     
     // MARK: - Actions
 }
 
+extension UITextField {
+    
+    func setLeftIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 40, y: 5, width: 20, height: 20))
+        iconView.image = image
+        iconView.tintColor = .systemGray2
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 80, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+    
+    func setRightIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 40, y: 5, width: 20, height: 20))
+        iconView.image = image
+        iconView.tintColor = .systemGreen
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 80, height: 30))
+        iconContainerView.addSubview(iconView)
+        rightView = iconContainerView
+        rightViewMode = .always
+    }
+    
+}
