453abe60c58c1001cb7fbfdbc114acd450f97365
RRDubina <dubinaroman.dubina@gmail.com>
2022-12-23 19:10:31 +0500

    Add text "Login"

--- IOS8-HW-11-DubinaRoman/Sourse/ViewController.swift
+++ IOS8-HW-11-DubinaRoman/Sourse/ViewController.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
     // MARK: - UI Elements
     
+    private lazy var labelLogin = {
+        let labelLogin = UILabel()
+        labelLogin.text = "Login"
+        labelLogin.textColor = .white
+        labelLogin.font = UIFont.boldSystemFont(ofSize: 40)
+        labelLogin.textAlignment = .center
+        return labelLogin
+    }()
     
     // MARK: - LifeCycle
     
@@ -18,20 +27,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setupView()
         setupHierarchy()
-        setupHierarchy()
+        setupLayout()
     }
     
     // MARK: - Setups
     
     private func setupView() {
+        view.backgroundColor = .blue
     }
     
     private func setupHierarchy() {
-        
+        view.addSubview(labelLogin)
     }
     
     private func setupLayout() {
         
+        labelLogin.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.top.equalTo(view).offset(100)
+        }
     }
     
     // MARK: - Actions
